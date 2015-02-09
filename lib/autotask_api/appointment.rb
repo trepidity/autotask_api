5d298d62242a839fa46fe24b3c39a295e2b0d48e
Jared Jennings <jared@jaredjennings.org>
2015-02-09 16:58:23 -0600

    class instance?

--- lib/autotask_api/appointment.rb
+++ lib/autotask_api/appointment.rb
@@ -3,11 +3,11 @@ module AutotaskAPI
     self.fields = [ :id, :title, :description, :start_date_time, :end_date_time ]
 
     def format_start
-      DateTime.strptime(:start_date_time, '%Y-%m-%dT%H:%M:%S')
+      DateTime.strptime(@start_date_time, '%Y-%m-%dT%H:%M:%S')
     end
 
     def format_end
-      DateTime.strptime(:end_date_time, '%Y-%m-%dT%H:%M:%S')
+      DateTime.strptime(@end_date_time, '%Y-%m-%dT%H:%M:%S')
     end
 
   end
