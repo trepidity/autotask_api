c7922fcddb1d8abdaff59e7a6f42ec0480826a4d
Jared Jennings <jared@jaredjennings.org>
2015-02-09 16:45:22 -0600

    Added methods to format. Needs to be fixed later.

--- lib/autotask_api/appointment.rb
+++ lib/autotask_api/appointment.rb
@@ -1,5 +1,14 @@
 module AutotaskAPI
   class Appointment < Entity
     self.fields = [ :id, :title, :description, :start_date_time, :end_date_time ]
+
+    def format_start
+      DateTime.strptime(:start_date_time, '%Y-%m-%dT%H:%M:%S')
+    end
+
+    def format_start
+      DateTime.strptime(:end_date_time, '%Y-%m-%dT%H:%M:%S')
+    end
+
   end
 end
