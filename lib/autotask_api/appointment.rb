e5bb991662d09fa66b12f43daf9b5592edc48c4b
Jared Jennings <jared@jaredjennings.org>
2015-03-01 19:04:09 -0600

    Added resource_id to Appointment Entity.

--- lib/autotask_api/appointment.rb
+++ lib/autotask_api/appointment.rb
@@ -1,5 +1,5 @@
 module AutotaskAPI
   class Appointment < Entity
-    self.fields = [ :id, :title, :description, :start_date_time, :end_date_time ]
+    self.fields = [ :id, :title, :description, :start_date_time, :end_date_time, :resource_id]
   end
 end
