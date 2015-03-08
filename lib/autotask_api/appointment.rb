4ea39e1cf8405a40d4a6fcfb49aa5bca73ec588d
Jared Jennings <jared@jaredjennings.org>
2015-03-07 22:22:06 -0600

    Added modified_date_time

--- lib/autotask_api/appointment.rb
+++ lib/autotask_api/appointment.rb
@@ -1,5 +1,5 @@
 module AutotaskAPI
   class Appointment < Entity
-    self.fields = [ :id, :title, :description, :start_date_time, :end_date_time, :resource_id]
+    self.fields = [ :id, :title, :description, :start_date_time, :end_date_time, :resource_id, :update_date_time]
   end
 end
--- lib/autotask_api/service_call.rb
+++ lib/autotask_api/service_call.rb
@@ -1,7 +1,7 @@
 module AutotaskAPI
   class ServiceCall < Entity
     self.fields = [ :id, :start_date_time, :end_date_time,
-                    :account_id, :description ]
+                    :account_id, :description, :last_modified_date_time ]
     has_one :service_call_ticket
   end
 end
