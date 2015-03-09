a18042e0def386638b460f704ac25eac85f9cd1d
Jared Jennings <jared@jaredjennings.org>
2015-03-09 14:51:02 -0500

    Added relationship to resource for appointment

--- lib/autotask_api/appointment.rb
+++ lib/autotask_api/appointment.rb
@@ -1,5 +1,6 @@
 module AutotaskAPI
   class Appointment < Entity
     self.fields = [ :id, :title, :description, :start_date_time, :end_date_time, :resource_id, :update_date_time]
+    belongs_to :resource
   end
 end
