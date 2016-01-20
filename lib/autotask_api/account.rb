9caaefaa8394bd50764ed00c791a3470aae98b24
Jared L Jennings <jared@jaredjennings.org>
2016-01-20 12:48:28 -0600

    Added additional fields to Account

--- lib/autotask_api/account.rb
+++ lib/autotask_api/account.rb
@@ -1,5 +1,5 @@
 module AutotaskAPI
   class Account < Entity
-    self.fields = [ :id, :account_name ]
+    self.fields = [ :id, :account_name, :address_1, :city, :country, :create_date, :phone, :postal_code, :state, :Active ]
   end
 end
