1e0db9153ce5b01089e44c18d43b97cb500b2c94
Jared L Jennings <jared@jaredjennings.org>
2016-01-20 12:52:04 -0600

    Additional Address fields for contact.

--- lib/autotask_api/contact.rb
+++ lib/autotask_api/contact.rb
@@ -1,6 +1,7 @@
 module AutotaskAPI
   class Contact < Entity
-    self.fields = [ :id, :first_name, :last_name, :phone, :e_mail_address, :mobile_phone, :account_id ]
+    self.fields = [ :id, :first_name, :last_name, :phone, :e_mail_address, :mobile_phone, :account_id, :address_line,
+                    :active, :city, :country, :state, :zip_code ]
 
     def full_name
       [ first_name, last_name ].join(' ')
